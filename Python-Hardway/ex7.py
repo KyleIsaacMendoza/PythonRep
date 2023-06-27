3c48b6af1a8620ea271b3dafc42b40f19dafb274
KyleIsaacMendoza <KyleIsaacMendoza24@gmail.com>
2023-06-27 20:04:29 +0800

    initial commit
    Learn Python the Hard way - Book

--- Python-Hardway/ex7.py
+++ Python-Hardway/ex7.py
@@ -17,7 +17,7 @@ end10 = "g"
 end11 = "e"
 end12 = "r"
 
-# watch that comma at the end. Try removing it to see what happens
+# watch that comma at the end. Try removing it to see what happens 
 
 print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
 print(end7 + end8 + end9 + end10 + end11 + end12)
