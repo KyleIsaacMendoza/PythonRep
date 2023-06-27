66f6dc633ed4c26cd650860196643a0b5b3e5868
KyleIsaacMendoza <KyleIsaacMendoza24@gmail.com>
2023-06-27 20:04:09 +0800

    initial commit
    Learn Python the Hard way - Book

--- Python-Hardway/ex9.py
+++ Python-Hardway/ex9.py
@@ -13,4 +13,4 @@ We'll be able to type as much as we like.
 Even 4 lines if we want, or 5, or 6.
 """) 
 
-# """ """ -> we'll save even the format of how you write it. 
+# """ """ -> we'll save even the format of how you write it.  
