f18de7814a69d7612297d48f25114a27cd724f85
KyleIsaacMendoza <KyleIsaacMendoza24@gmail.com>
2023-06-27 20:04:51 +0800

    initial commit
    Learn Python the Hard way - Book

--- Python-Hardway/ex5.py
+++ Python-Hardway/ex5.py
@@ -22,7 +22,7 @@ print(f"He's got {eyes} eyes and {hair} hair.")
 print(f"His teeth are usually {teeth} depending on the coffee.")
 
 
-# This line is tricky, try to get it exactly right
+# This line is tricky, try to get it exactly right 
 
 total = age + height + weight
 print(f"If i add {age}, {height}, and {weight} I get {total}.")
